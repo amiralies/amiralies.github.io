396c956b762fd1f092a87aa447b3f9ffc8a7c529
Amirali Esmaeili <esmailiamirali@gmail.com>
2019-07-04 02:43:28 +0430

    Upgrade deps

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const Container = props => <div className="container">{ props.children }</div>;
+const Container = ({ children }) => <div className="container">{children}</div>;
 
 export default Container;
